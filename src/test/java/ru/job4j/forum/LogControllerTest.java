e87d5e78dce4d67b73c407cae44707a70dad4f2d
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2020-08-26 12:37:06 +0300

    Spring mock [#2096]

--- src/test/java/ru/job4j/forum/LogControllerTest.java
+++ src/test/java/ru/job4j/forum/LogControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
 /**
@@ -20,6 +21,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(classes = ForumApplication.class)
 @AutoConfigureMockMvc
+@WithUserDetails("root")
 public class LogControllerTest {
 
     @Autowired
